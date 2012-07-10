3a32cd2b9a6e6298d322e478cdd6c3f55520a77d
Diego FV <elfogris@gmail.com>
2012-07-10 13:42:22 -0700

    adding stuff

--- myWeather/mojits/Master/binders/index.js
+++ myWeather/mojits/Master/binders/index.js
@@ -34,30 +34,66 @@ YUI.add('MasterBinderIndex', function(Y, NAME) {
          */
         bind: function(node) {
             this.node = node;
-            alert(1);
-
-            /*this.getCurrentLocation({
+            this.getCurrentLocation({
                 context:this,
-                onSuccess: function (){
-                    console.log(arguments);
-                }
-            });*/
+                onSuccess: Y.bind(this.onSuccessGetLocation,this)
+            });
         },
         getCurrentLocation:function(config){
             var userSuccess = config.context === 'undefined' ? config.onSuccess : Y.bind(config.onSuccess,config.context),
-                useCache = config.cache === true,
                 success = Y.bind(userSuccess,this),
                 error =  typeof config.onError === 'function' ? config.onError : function(evt){Y.log(evt);},
                 options = typeof config.options !== 'undefined' ? config.options : {enableHighAccuracy:true};
-            if(useCache && this.isLocationCached()){
-                Y.log("Take cache");
-                return userSuccess(this.cachedLocation);
-            }else{
-                Y.log("Getting location");
                 navigator.geolocation.getCurrentPosition(success,error,options);
-            }
+        },
+        onSuccessGetLocation: function (geoPosition) {
+            this.mojitProxy.invoke('getWeatherJSON', Y.bind(this.refreshWeatherView, this));
+        },
+        refreshWeatherView: function (err, weather) {
+            var name = '.weather',
+                placeSel ="#placeName",
+                place = weather.location.city +' ('+weather.location.country+')',
+                tempSel = name + " #temp",
+                temp = weather.item.condition.temp + " ºC",
+                imgSel =name + " .image",
+                img = '<img src="http://l.yimg.com/a/i/us/nws/weather/gr/'+ weather.item.condition.code+'d.png">',
+                descSel = name + " .desc",
+                desc = weather.item.condition.text,
+                forecastSel= name + " .forecast",
+                fNode = weather.item.forecast,
+                forecast = "",i;
+                for (i=0; i<fNode.length ;i++) {
+                    forecast+= "<p>"+ fNode[i].day +': '+ fNode[i].text+ ' <span>(H: '+fNode[i].high+' | L: '+fNode[i].low+')</span></p>';
+                }
+            Y.one(placeSel).setContent(place);
+            Y.one(tempSel).setContent(temp);
+            Y.one(imgSel).setContent(img).setStyles({"background":"none"});
+            Y.one(descSel).setContent(desc);
+            Y.one(forecastSel).setContent(forecast);
+
+            //LANDSCAPE
+            var sunrise = weather.astronomy.sunrise,
+                sunset = weather.astronomy.sunset,
+                humidity = weather.atmosphere.humidity,
+                pressure = weather.atmosphere.pressure,
+                visibility = weather.atmosphere.visibility,
+                chill = weather.wind.chill,
+                direction = weather.wind.direction,
+                speed = weather.wind.speed;
+            //astronomy   
+            Y.one("#wSunrise").setContent(sunrise);
+            Y.one("#wSunset").setContent(sunset);
+            //atmosphere
+            Y.one("#wHum").setContent(humidity);
+            Y.one("#wPress").setContent(pressure);
+            Y.one("#wVis").setContent(visibility);
+            //wind
+            Y.one("#wChill").setContent(chill);
+            Y.one("#wDir").setContent(direction);
+            Y.one("#wSpeed").setContent(speed);
+
         }
 
     };
 
-}, '0.0.1', {requires: ['event-mouseenter', 'mojito-client']});
+}, '0.0.1', {requires: ['event-mouseenter', 'mojito-client','node']});
--- myWeather/mojits/Master/controller.server.js
+++ myWeather/mojits/Master/controller.server.js
@@ -28,25 +28,27 @@ YUI.add('Master', function(Y, NAME) {
          * @param ac {Object} The ActionContext that provides access
          *        to the Mojito API.
          */
+
         index: function(ac) {
-            var data = {'x': 'y'};
-            //ac.assets.addBlob('<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">','top');
-                ac.assets.addCss('./index.css');
-                ac.done({
-                    status: 'Mojito is working.',
-                    data: data
-                });
+            ac.assets.addBlob('<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">','top');
+            ac.assets.addCss('./index.css');
+            ac.done({
+                status: 'Weather!'
+            });
         },
-        getWeather: function(ac) {
-            ac.models.MasterModelWeatherYQL.getData(function(err, data) {
+        getWeatherJSON: function(ac) {
+            ac.models.MasterModelWeatherYQL.getData({geo:'data'}, function (err, data) {
                 if (err) {
                     ac.error(err);
                     return;
                 }
-
+                ac.done(data, 'json');
             });
+        },
+        getWeather: function () {
+            
         }
 
     };
 
-}, '0.0.1', {requires: ['mojito', 'MasterModelFoo']});
+}, '0.0.1', {requires: ['mojito', 'MasterModelWeatherYQL']});
